b9eafe09a433661c16cef9cf83830e3e416aa94f
Subrat Kumar Barik <subratmca85@gmail.com>
2025-07-01 21:50:24 +0530

    re-structure web page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,164 @@
 import React from 'react';
 import './App.css';
-import Menu from './components/Menu';
-import Form from './components/Form';
 
 function App() {
-  return (
-    <>
-      <Menu logo="Company Logo"/>
-        <div className='container'>
-          <div className='row'>
-            <Form kelma="This is form for testing"/>
-          </div>
-        </div>
-      </>
-  );
+	return (
+		<>
+			
+			<div className="header">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3">
+							<div style={{ height: '150px', background: 'limegreen' }}></div>
+						</div>
+						<div className="col-md-9">							
+							<div className="row">
+								<div className="col-md-12">
+									<div style={{ height: '69px', background: 'limegreen', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '69px', background: 'limegreen' }}></div>
+								</div>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</div> {/* Header Ends */}
+
+			<div className="asset-scrollar">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<div style={{ height: '50px', background: 'LightSeaGreen' }}></div>
+						</div>
+					</div>
+				</div>
+			</div> {/* Asset Scrollar Ends */}
+
+			<div className="banner-section">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6">
+							<div style={{ height: '350px', background: 'coral' }}></div>
+						</div>
+						<div className="col-md-6">
+							<div className="row">
+								<div className="col-md-12">
+									<div style={{ height: '110px', background: 'coral', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '110px', background: 'coral', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '110px', background: 'coral', marginBottom: '10px' }}></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> {/* Banner Section Ends */}
+
+			<div className="blog-section">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8">
+							<div className="row">
+								<div className="col-md-12">
+									<div style={{ height: '180px', background: 'violet', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '180px', background: 'violet', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '180px', background: 'violet', marginBottom: '10px' }}></div>
+								</div>
+								<div className="col-md-12">
+									<div style={{ height: '180px', background: 'violet', marginBottom: '10px' }}></div>
+								</div>
+							</div>
+						</div>
+						<div className="col-md-4">
+							<div className="col-md-12">
+								<div style={{ height: '117px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+							<div className="col-md-12">
+								<div style={{ height: '117px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+							<div className="col-md-12">
+								<div style={{ height: '117px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+							<div className="col-md-12">
+								<div style={{ height: '117px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+							<div className="col-md-12">
+								<div style={{ height: '117px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+							<div className="col-md-12">
+								<div style={{ height: '114px', background: 'violet', marginBottom: '10px' }}></div>
+							</div>
+						</div>
+					</div>	
+				</div>	
+			</div> {/* Banner Section Ends */}
+
+			<div className="blog-scrollar">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-3">
+							<div style={{ height: '150px', background: 'salmon', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-3">
+							<div style={{ height: '150px', background: 'salmon', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-3">
+							<div style={{ height: '150px', background: 'salmon', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-3">
+							<div style={{ height: '150px', background: 'salmon', marginBottom: '10px' }}></div>
+						</div>
+					</div>
+				</div>
+			</div> {/* Banner Scrollar Ends */}
+
+			<div className="table-section">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8">
+							<div style={{ height: '150px', background: 'Turquoise', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-4">
+							<div style={{ height: '150px', background: 'Turquoise', marginBottom: '10px' }}></div>
+						</div>
+					</div>
+				</div>
+			</div> {/* Banner Scrollar Ends Turquoise */}
+
+			<div className="footer">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-4">
+							<div style={{ height: '150px', background: 'tan', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-4">
+							<div style={{ height: '150px', background: 'tan', marginBottom: '10px' }}></div>
+						</div>
+						<div className="col-md-4">
+							<div style={{ height: '150px', background: 'tan', marginBottom: '10px' }}></div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-6">
+							<div style={{ height: '50px', background: 'tan' }}></div>
+						</div>
+						<div className="col-md-6">
+							<div style={{ height: '50px', background: 'tan' }}></div>
+						</div>
+					</div>
+				</div>
+			</div> {/* Footer Ends */}
+
+		</>
+	);
 }
 
 export default App;
